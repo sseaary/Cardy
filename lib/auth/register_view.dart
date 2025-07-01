77db8933f538634b2260899040bb4cf439dcdfa3
Sea <sea.panaporn@gmail.com>
2025-07-01 08:53:23 +0700

    add scroller to register, login pages

--- lib/auth/register_view.dart
+++ lib/auth/register_view.dart
@@ -16,6 +16,8 @@ class _RegisterState extends State<Register> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  final controller = ScrollController();
+
   Future<void> registerUser() async {
     try {
       final auth = FirebaseAuth.instance;
@@ -54,138 +56,157 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
+    double keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
     return Scaffold(
-      // backgroundColor: Color(0xFF0D243D),
-      // appBar: AppBar(title: Text('Register')),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            // begin: Alignment.topLeft,
-            // end: Alignment.bottomRight,
-            begin: Alignment(-0.817, -0.576),
-            end: Alignment(0.817, 0.576),
-            colors: [Color(0xFF2E82DB), Color(0xFF0D243D)],
-            stops: [0.3, 0.3], // ให้สีเปลี่ยนทันทีที่ครึ่งทาง
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(-0.817, -0.576),
+                end: Alignment(0.817, 0.576),
+                colors: [Color(0xFF2E82DB), Color(0xFF0D243D)],
+                stops: [0.3, 0.3], // ให้สีเปลี่ยนทันทีที่ครึ่งทาง
+              ),
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 160),
-            Container(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                child: Text(
-                  'Create an account',
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 80),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-              child: TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: "Name",
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-              child: TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: "Email",
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-              child: TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: "password",
-                ),
-              ),
-            ),
-            SizedBox(height: 60),
-            SizedBox(
-              width: 200,
-              height: 48,
-              child: ElevatedButton(
-                onPressed: () {
-                  registerUser();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF2E82DB),
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(16),
-                  ),
-                ),
-                child: Text(
-                  "Register",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-              ),
-            ),
-            SizedBox(height: 60),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'Already have an account? ',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w300,
+          SingleChildScrollView(
+            controller: controller,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 160),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                    child: Text(
+                      'Create an account',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800,
+                      ),
                     ),
                   ),
-                  TextSpan(
-                    text: ' Login',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.pink,
-                      fontWeight: FontWeight.w300,
-                      decoration: TextDecoration.underline,
+                ),
+                SizedBox(height: 80),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: TextField(
+                    controller: _nameController,
+
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide.none,
+                      ),
+                      hintText: "Name",
                     ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Get.toNamed("/");
-                      },
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (value) {
+                      controller.jumpTo(50);
+                    },
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide.none,
+                      ),
+                      hintText: "Email",
+                    ),
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (value) {
+                      controller.jumpTo(100);
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide.none,
+                      ),
+                      hintText: "password",
+                    ),
+                    textInputAction: TextInputAction.next,
+                  ),
+                ),
+                SizedBox(height: 60),
+                SizedBox(
+                  width: 200,
+                  height: 48,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      registerUser();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF2E82DB),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(16),
+                      ),
+                    ),
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 60),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Already have an account? ',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' Login',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.pink,
+                          fontWeight: FontWeight.w300,
+                          decoration: TextDecoration.underline,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Get.toNamed("/");
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: keyboardHeight),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

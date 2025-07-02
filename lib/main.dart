465bee222a4f84b3ced36dddc65ef70634847ee9
Sea <sea.panaporn@gmail.com>
2025-07-02 18:46:16 +0700

    clean main page

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       initialRoute: Storage().islogin() ? "/home" : "/",
-      // initialRoute: "/addVocabScreen",
-      // initialRoute: "/wordFormPage",
+
       getPages: [
         GetPage(name: "/", page: () => Login()),
         GetPage(name: "/register", page: () => Register()),
@@ -41,20 +40,20 @@ class MyApp extends StatelessWidget {
         GetPage(name: "/newGame", page: () => NewGame()),
         GetPage(name: "/favoriteView", page: () => FavoriteView()),
 
-        GetPage(
-          name: "/wordFormPage",
-          page: () => WordFormPage(
-            docId: 'F2q8jheorvzlnFaTp3D6',
-            wordData: {
-              "words": "sss",
-              "level": "sea",
-              "description": "wwww",
-              "image_url":
-                  "https://res.cloudinary.com/dhzgzeoqe/image/upload/v1751446408/nojlrxhn2jkddktiyac0.jpg",
-              "created_by": "fUiMB47J9VczJz0rYHui0aWUFox1",
-            },
-          ),
-        ),
+        // GetPage(
+        //   name: "/wordFormPage",
+        //   page: () => WordFormPage(
+        //     docId: 'F2q8jheorvzlnFaTp3D6',
+        //     wordData: {
+        //       "words": "sss",
+        //       "level": "sea",
+        //       "description": "wwww",
+        //       "image_url":
+        //           "https://res.cloudinary.com/dhzgzeoqe/image/upload/v1751446408/nojlrxhn2jkddktiyac0.jpg",
+        //       "created_by": "fUiMB47J9VczJz0rYHui0aWUFox1",
+        //     },
+        // ),
+        // ),
       ],
     );
   }
